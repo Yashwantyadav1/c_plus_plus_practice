8ce52283f5a488f33e5b741f95b5aeb65c16217a
Yashwantyadav1 <yy888275@gmail.com>
2023-10-15 21:23:21 +0530

    practice programs

---
+++ BASICS/6_practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string firstname="yashwant";
+    string secondname="yadav";
+    string fullname=
+    firstname.append(secondname);
+    cout<<fullname;
+    return 0;
+
+}
